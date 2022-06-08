146f5ce00ffa869de47d411a59149311cbe39031
Andrey Nelyubov <andrey.nelyubov@westpac.co.nz>
2022-06-08 15:40:57 +1200

    added ejs

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
-const { MainClient } = require('binance');
-const client = new MainClient();
+app.set('view engine', 'ejs');;
+app.listen(3000);
+//const { MainClient } = require('binance');
+//const client = new MainClient();
 /*{
   api_key: API_KEY,
   api_secret: API_SECRET,
 });*/
-
+/*
 client.getExchangeInfo()
   .then(result => {
     console.log("getExchangeInfo inverse result: ", result);
   })
   .catch(err => {
     console.error("getExchangeInfo inverse error: ", err);
+  });*/
+
+  app.get('/', function(req, res) {
+    res.render('pages/index');
+  });
+
+  app.get('/bina', function(req, res) {
+    res.render('pages/index');
   });
